498a2817decc1c7842787d51f5faf1102be7bd6c
vonplaza <vonpaulop@gmail.com>
2023-04-16 23:37:09 +0800

    fresh-branch

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,25 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { MaterialModule } from '../shared/material';
-import { dashboard } from './dashboard.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { ChartModule } from 'angular-highcharts';
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { NgChartsModule } from 'ng2-charts';
-
 
 
 @NgModule({
   declarations: [
-    dashboard
   ],
   imports: [
     CommonModule,
     SharedModule,
     MaterialModule,
     ChartModule,
-    NgChartsModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
   ]
 })
 export class DashboardModule { }
