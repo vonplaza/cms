7d55203050eb0ae0654d3c56a7a43e0cdb2914ea
Kervin Pagtalunan <121629411+kervinpagtalunan13@users.noreply.github.com>
2023-04-23 23:25:56 +0800

    with guard

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountListComponent2 } from './components/account-list/account-list.component';
 import {AccountContainerComponent} from './components/account-container/account-container.component'
 import { AccountRegistrationComponent } from './components/account-registration/account-registration.component';
+import { AuthGuard } from '../core/guard/auth.guard';
 
 
 
 const routes: Routes = [
   { path: '', component: AccountContainerComponent,
+    canActivate: [AuthGuard],
     children: [
       { path: '', component: AccountListComponent2 },
       { path: 'register', component: AccountRegistrationComponent },
---
+++ src/app/core/guard/login.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, NavigationEnd, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable, filter } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginGuard implements CanActivate {
+  constructor(private authService: AuthService, 
+              private router: Router,
+    ){
+    }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+    return new Observable(obs => {
+      this.authService.getCurrentUser()
+        .subscribe({
+          next: data => {
+            if(!data){
+              obs.next(true)
+            }
+            this.router.navigate(['/', 'dashboard'])
+            obs.next(false)
+          },
+          
+          error: err => {
+            this.router.navigate(['/', 'login'])
+            obs.next(false)
+          }
+        })
+    })  
+
+  }
+  
+}
