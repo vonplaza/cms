2a07951deef394026b99da6d54cfa2028ff648b1
vonplaza <vonpaulop@gmail.com>
2023-04-24 23:57:19 +0800

    putangina sa wakas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent{
+  // madilimBa:boolean | undefined;
+  // ngDoCheck(): void {
+  //   if(!this.madilimBa){
+  //     if (this.body) {
+  //            this.body.classList.add('theme-light');
+  //            this.body.classList.remove('theme-dark');
+  //            console.log('putaninang yan'+this.madilimBa);
+  //        }
+  //      }
+  //      else if(this.madilimBa){
+  //        if (this.body) {
+  //          this.body.classList.add('theme-dark');
+  //          this.body.classList.remove('theme-light');
+  //          console.log('putaninang mo');
+  //      }
+  //      }
+  // }
+  // ngOnInit(): void {
+  //   if(!this.madilimBa){
+  //    if (this.body) {
+  //           this.body.classList.add('theme-light');
+  //           this.body.classList.remove('theme-dark');
+  //           //console.log('set as light');
+  //       }
+  //     }
+  //     else if(this.madilimBa){
+  //       if (this.body) {
+  //         this.body.classList.add('theme-dark');
+  //         this.body.classList.remove('theme-light');
+  //         //console.log('set as light');
+  //     }
+  //     }
+  // }
   title = 'cict-curriculum-system';
    body = document.querySelector('body');
-   
-  toggleDarkMode(){
-    
-    if (this.body) {
-            this.body.classList.add('theme-dark');
-            this.body.classList.remove('theme-light');
-            console.log('set as dark');
-            
-        
-    }
-  }
-
-  toggleLightMode(){
-    
-    if (this.body) {
-            this.body.classList.add('theme-light');
-            this.body.classList.remove('theme-dark');
-            console.log('set as light');
-        }
-    }
   }
   
 
